6ec8361d3df7c6235ea3b87fbe7e2b31453f9772
Olga Verciuc <o.verciuc@gmail.com>
2021-05-04 14:50:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,9 +12,12 @@ const color = randomColor({
 function colorShow(str) {
     console.log(chalk.hex(color).bold(str));
 }
-
-colorShow(
-    `##############################
+if (process.argv[2] === 'ask') {
+    console.log('What color would you like to see?');
+    console.log('What luminosity would you like to see?');
+} else {
+    colorShow(
+        `##############################
 ##############################
 ##############################
 ########              ########
@@ -23,4 +26,5 @@ colorShow(
 ##############################
 ##############################
 ##############################`,
-);
+    );
+}
